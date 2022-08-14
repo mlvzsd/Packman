9ae9260c9ceb3538fd8c6f22d4396c9b6ede292b
chonkachu <chrissimascgr@gmail.com>
2022-08-14 13:58:50 -0300

    adding input from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
+use std::io;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Parses{
     Value,
     Pack
@@ -8,12 +9,13 @@ enum Parses{
 
 #[derive(Debug)]
 struct Config {
-    pack_size: u32
+    pack_size: u32,
+    from_stdin: bool
 }
 
 fn main() {
     let mut aux = Parses::Value;
-    let mut config = Config{pack_size : 64}; 
+    let mut config = Config{pack_size : 64, from_stdin : false}; 
     let mut vals = Vec::<u32>::new();
 
     for i in env::args().skip(1) { // traverse the args for parameters
@@ -21,14 +23,33 @@ fn main() {
             aux = Parses::Pack;
             continue;
         }
+        else if i == "-i" {
+            config.from_stdin = true;
+            continue;
+        }
 
         match aux {
-            Parses::Pack => {
-                config.pack_size = i.parse::<u32>().expect("Pack value must be a positive integer");
-            },
+            Parses::Pack => config.pack_size = i.parse::<u32>().expect("Pack value must be a positive integer"),
             Parses::Value => vals.push(i.parse::<u32>().expect("Pack value must be a positive integer"))
         }
         
+    }
+    if config.from_stdin {
+        loop {
+            let mut input = String::new();
+
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    if !input.is_empty() {
+                        vals.push( input.trim().parse::<u32>().expect("Pack Value must be positive integers"));
+                    }
+                    else {
+                        break;
+                    }
+                },
+                Err(_) =>  {}
+            }
+        } 
     }
     for i in vals {
         let quotient = i/config.pack_size; 
